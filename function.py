dc8fd596450421cb54342d4fd70a6fe7502e91cb
Abhiuday Narayan <narayanabhiuday@gmail.com>
2025-05-22 21:09:52 +0530

    Function

---
+++ function.py
@@ -0,0 +1,13 @@
+# def func_name (param1 , param2 , ...) :
+# some work
+# return val
+
+#func_name (arg1,arg2...) function call
+
+'''def sum (a,b):
+    return a+b
+p = int(input("enter the number_1"))
+n = int(input("enter the number_2"))
+print(sum(p,n))'''
+
+
