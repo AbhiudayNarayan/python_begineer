4e344a4d0a060602f4d02458954ae92e06a7d2b7
Abhiuday Narayan <narayanabhiuday@gmail.com>
2025-05-23 13:55:30 +0530

    recursion update

--- recurssion.py
+++ recurssion.py
@@ -4,3 +4,15 @@
     else:
         return n * factorial(n-1)
 print(factorial(5))'''
+
+'''def firs_n_sum(n):
+    if n == 0:
+        return 0
+    else :
+        return n + firs_n_sum(n-1)
+
+k=int(input("enter the number"))
+print(firs_n_sum(k))
+a = k*(k+1)/2
+print(a)
+print(firs_n_sum(k))'''
